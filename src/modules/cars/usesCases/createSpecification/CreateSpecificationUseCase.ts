bec791a9fb16d54b4d82997a1088ae25f6f4fabb
welitoonl <69065140+welitoonl@users.noreply.github.com>
2022-03-27 22:31:16 -0300

    up all

---
+++ src/modules/cars/usesCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,25 @@
+import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification Already Exists!");
+    }
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import multer from "multer";
+
+import createCategoryController from "../modules/cars/usesCases/createCategory";
+import { importCategoryController } from "../modules/cars/usesCases/importCategory";
+import { listCategoriesController } from "../modules/cars/usesCases/listCategories";
+
+const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
+
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController().handle(request, response);
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
+});
+
+export { categoriesRoutes };
